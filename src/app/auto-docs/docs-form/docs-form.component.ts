6485b12f176faf9956ecec5254bffc66070eda85
MKPC <MKPC@MKPCs-MacBook-Pro.local>
2019-12-12 10:34:54 -0800

    added doc forms

--- src/app/auto-docs/docs-form/docs-form.component.ts
+++ src/app/auto-docs/docs-form/docs-form.component.ts
@@ -13,23 +13,24 @@ import { ApplicationService } from 'src/app/services/application.service';
 export class DocsFormComponent implements OnInit {
 
   @Output() submitForm = new EventEmitter();
-
+  flag:boolean = false;
   constructor(private applicationService: ApplicationService) {
     this.masterSelected = false;
     this.checklist = [
       { id: 1, value: 'CRE Commitment Letter', isSelected: false },
-      { id: 2, value: 'Document B', isSelected: false },
-      { id: 3, value: 'Document C', isSelected: false },
-      { id: 4, value: 'Document D', isSelected: false }
+      { id: 2, value: 'RES ACP Commitment Letter', isSelected: false },
+      // { id: 3, value: 'Document C', isSelected: false },
+      // { id: 4, value: 'Document D', isSelected: false }
     ];
     this.checkedList = [];
     this.getCheckedItemList();
   }
 
   formTypes = ['Lending', 'Servicing', 'Deposits', 'Employee'];
-
-  formSearchList: any = ['CRE Com'];
-  loanNumSearchList: any = ['60-123456', 'John A. Doe'];
+  loanNumSearchList: any = ['60-123456', '60-123457','60-123458','60-123459','60-44444','60-4444234'];
+  
+  formSearchList: any = ['60-123456', '60-123457','60-123458','60-123459','60-44444','60-4444234'];
+  
   model = {
     formType: '',
     formSearch: '',
@@ -76,13 +77,18 @@ export class DocsFormComponent implements OnInit {
     getCheckedItemList(){
       this.checkedList = [];
       for (var i = 0; i < this.checklist.length; i++) {
-        if(this.checklist[i].isSelected)
-        this.checkedList.push(this.checklist[i]);
+        if(this.checklist[1].isSelected) {
+          this.checkedList.push(this.checklist[0]);
+        }
+        else if(this.checklist[0].isSelected) {
+          this.checkedList.push(this.checklist[1]);
+        }
       }
       this.checkedList = JSON.stringify(this.checkedList);
     }
 
   onSubmit() {
+    console.log(this.checklist[0]);
     this.submitted = true;
     const inputReqObj =  {
       formType: this.model.formType,
@@ -90,10 +96,20 @@ export class DocsFormComponent implements OnInit {
       loanNumber: this.model.loanNumber,
       selectedDocs: this.checkedList
     };
-    this.applicationService.getDoc(inputReqObj).subscribe(data => {
-      this.docLink = data.docLink;
+    // this.applicationService.getDoc(inputReqObj).subscribe(data => {
+    //   this.docLink = data.docLink;
+    //   this.submitForm.emit(this.docLink);
+    // });
+
+    this.applicationService.getFormList().subscribe(data => {
+      console.log(data.autoDocs[0].doclink)
+      if(this.checklist[0].isSelected) {
+        this.docLink = data.autoDocs[0].doclink[1].value;
+      } else {
+        this.docLink = data.autoDocs[0].doclink[0].value;
+      }
       this.submitForm.emit(this.docLink);
-    });
+    })
   }
 
   ngOnInit() {
@@ -103,6 +119,22 @@ export class DocsFormComponent implements OnInit {
     //  // this.formList = data.autoDocs[0].formType;
     //   console.log(this.formList);
     // });
+  }
+
+  onSelection(value) {
+    
+    switch(value) {
+        case 'formtypevalue': 
+            console.log(value);
+            this.changeSelection();
+            break; 
+        
+    }
+
+  }
+
+  changeSelection(){
+    this.flag = true;
   }
 
 }
